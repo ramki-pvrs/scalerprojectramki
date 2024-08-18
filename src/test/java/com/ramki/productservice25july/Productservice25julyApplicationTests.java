6664a664bef97cb7813bebb1421bfca82a5d78a5
Ramakrishnan Srinivasan <ramki.pvrs@gmail.com>
2024-08-18 14:33:03 +0530

    Init

---
+++ src/test/java/com/ramki/productservice25july/Productservice25julyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramki.productservice25july;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productservice25julyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
